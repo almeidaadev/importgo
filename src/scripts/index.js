e638c0e261140e1e51d44f713f329962795d2203
almeidaadev <unkn0wn01127800@gmail.com>
2024-06-22 18:16:17 -0300

    menu

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,26 +1,11 @@
-// const menuBurguer = document.querySelector(".menu-burguer");
-
-// menuBurguer.addEventListener("click", function () {
-//   this.classList.toggle("active");
-//   let atributoMenu = menuBurguer.getAttribute("data-menu");
-//   atributoMenu =
-//   if (!atributoMenu) {
-//     console.log('n tem nada')
-//   } else {
-//     console.log(atributoMenu)
-//   }
-// });
-
-const iconMenu = document.querySelector(".menu-burguer");
 const dataList = document.querySelector("[data-list-header]");
 
-
-iconMenu.addEventListener("click", function () {
-  if (this.classList.contains("open")) {
-    this.classList.remove("open");
-    dataList.style.display = "block"
-  } else {
-    this.classList.add("open");
-    dataList.style.display = "none"
-  }
-}, false);
+$(function () {
+  $(".menu-burguer").click(function () {
+    if (this.classList.contains("open")) {
+      this.classList.remove("open");
+    } else {
+      this.classList.add("open");
+    }
+  });
+});
