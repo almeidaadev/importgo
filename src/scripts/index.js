df8c65ed66620a20071c5fe1390a8e730525868c
almeidaadev <unkn0wn01127800@gmail.com>
2024-06-22 18:46:16 -0300

    tamo junto thiago

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,16 +1,12 @@
 const iconMenu = document.querySelector(".menu-burguer");
 const dataList = document.querySelector("[data-list-header]");
 
-dataList.style.display = "none";
-
 $(function () {
   $(".menu-burguer").click(function () {
     if (this.classList.contains("open")) {
       this.classList.remove("open");
-      dataList.style.display = "none";
     } else {
-      this.classList.add("open");
-      dataList.style.display = "block";
+      this.classList.add("open"); 
     }
   });
 });
