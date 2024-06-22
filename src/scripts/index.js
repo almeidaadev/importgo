94cc02dd73b2e4a6663d83c38a2317908bc7f03d
almeidaadev <unkn0wn01127800@gmail.com>
2024-06-22 16:08:07 -0300

    menu in progress

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,12 +1,26 @@
-const menuBurguer = document.querySelector(".menu-burguer");
+// const menuBurguer = document.querySelector(".menu-burguer");
 
-menuBurguer.addEventListener("click", function () {
-  this.classList.toggle("active");
-  let atributoMenu = menuBurguer.getAttribute("data-menu");
-  atributoMenu = 
-  if (!atributoMenu) {
-    console.log('n tem nada')
+// menuBurguer.addEventListener("click", function () {
+//   this.classList.toggle("active");
+//   let atributoMenu = menuBurguer.getAttribute("data-menu");
+//   atributoMenu =
+//   if (!atributoMenu) {
+//     console.log('n tem nada')
+//   } else {
+//     console.log(atributoMenu)
+//   }
+// });
+
+const iconMenu = document.querySelector(".menu-burguer");
+const dataList = document.querySelector("[data-list-header]");
+
+
+iconMenu.addEventListener("click", function () {
+  if (this.classList.contains("open")) {
+    this.classList.remove("open");
+    dataList.style.display = "block"
   } else {
-    console.log(atributoMenu)
+    this.classList.add("open");
+    dataList.style.display = "none"
   }
-});
+}, false);
