463f2fa61f3bf2df7a56e992f20fb0f410d2ff91
almeidaadev <unkn0wn01127800@gmail.com>
2024-06-22 18:19:55 -0300

    menu finished

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,11 +1,16 @@
+const iconMenu = document.querySelector(".menu-burguer");
 const dataList = document.querySelector("[data-list-header]");
+
+dataList.style.display = "none";
 
 $(function () {
   $(".menu-burguer").click(function () {
     if (this.classList.contains("open")) {
       this.classList.remove("open");
+      dataList.style.display = "none";
     } else {
       this.classList.add("open");
+      dataList.style.display = "block";
     }
   });
 });
