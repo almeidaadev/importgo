cae7f1dc58aa16d9cc1ecf118a9acdf4e1a11dd1
almeidaadev <unkn0wn01127800@gmail.com>
2024-06-22 13:13:45 -0300

    fazendo menu burguer

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,5 +1,12 @@
 const menuBurguer = document.querySelector(".menu-burguer");
 
-menuBurguer.addEventListener("click", function() {
-    this.classList.toggle("active")
-}) 
+menuBurguer.addEventListener("click", function () {
+  this.classList.toggle("active");
+  let atributoMenu = menuBurguer.getAttribute("data-menu");
+  atributoMenu = 
+  if (!atributoMenu) {
+    console.log('n tem nada')
+  } else {
+    console.log(atributoMenu)
+  }
+});
