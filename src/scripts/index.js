9eb76e15e01688a2da3c78f2cdb365078eebcf44
almeidaadev <unkn0wn01127800@gmail.com>
2024-06-23 03:10:02 -0300

    bug menu resolvido

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,39 @@
 import header from "./header.js";
 
 header();
+
+
+
+export default function menu() {
+  const dataList = document.querySelector("[data-list-header]");
+  const dataNavigationHeader = document.querySelector(
+    "[data-navigation-header]"
+  );
+
+  $(function () {
+    $(".menu-burguer").click(function () {
+      $(this).toggleClass("open");
+
+      dataList.style.display = "none";
+
+      if ($(this).hasClass("open")) {
+        dataList.style.display = "flex";
+        dataNavigationHeader.style.display = "block";
+      } else {
+        dataList.style.display = "none";
+        dataNavigationHeader.style.display = "none";
+      }
+    });
+
+    $(window).resize(function () {
+      if ($(window).width() > 900) {
+        dataList.style.display = "flex";
+        dataNavigationHeader.style.display = "block";
+      } else {
+        dataList.style.display = "none";
+        dataNavigationHeader.style.display = "none";
+        $("menu-burguer").removeClass("open");
+      }
+    });
+  });
+}
