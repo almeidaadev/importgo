a64ed6daafad0ad018828e0f02b927b4b920026f
almeidaadev <unkn0wn01127800@gmail.com>
2024-07-11 20:33:26 -0300

    up

--- src/scripts/contact.js
+++ src/scripts/contact.js
@@ -1,19 +1,19 @@
-export default function contact() {
-  document.addEventListener("DOMContentLoaded", function (e) {
-    e.preventDefault();
+// export default function contact() {
+//   document.addEventListener("DOMContentLoaded", function (e) {
+//     e.preventDefault();
 
-    const dropdown = document.querySelector(".dropdown");
-    const dropdownContent = document.querySelector(".dropdown-content");
-    const event = ["mouseenter", "click"];
+//     const dropdown = document.querySelector(".dropdown");
+//     const dropdownContent = document.querySelector(".dropdown-content");
+//     const event = ["mouseenter", "click"];
 
-    event.forEach((ev) => {
-      dropdown.addEventListener(ev, function () {
-        dropdownContent.style.display = "block";
-      });
-    });
+//     event.forEach((ev) => {
+//       dropdown.addEventListener(ev, function () {
+//         dropdownContent.style.display = "block";
+//       });
+//     });
 
-    dropdown.addEventListener("mouseleave", function () {
-      dropdownContent.style.display = "none";
-    });
-  });
-}
+//     dropdown.addEventListener("mouseleave", function () {
+//       dropdownContent.style.display = "none";
+//     });
+//   });
+// }
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,15 +1,17 @@
+// import contact from "./contact.js";
+// contact();
+
+
 import initAnimacaoScroll from "./animationScroll.js";
 import header from "./header.js";
 import softscroll from "./softSroll.js";
 import efectHeader from "./efectHeader.js";
 import internet from "./internet.js";
 import initFuncionamento from "./funcionamento.js";
-import contact from "./contact.js";
 
 header();
 softscroll();
 initAnimacaoScroll();
 efectHeader();
 initFuncionamento();
-contact();
 internet();
