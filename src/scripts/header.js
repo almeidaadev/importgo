44b624856040e7c6b98a5c8f4214d8a8393374ae
almeidaadev <unkn0wn01127800@gmail.com>
2024-07-11 20:33:12 -0300

    Add click outside menuburguer

--- src/scripts/header.js
+++ src/scripts/header.js
@@ -1,11 +1,12 @@
 export default function menu() {
   const dataList = document.querySelector("[data-list-header]");
-  const dataNavigationHeader = document.querySelector(
-    "[data-navigation-header]"
-  );
+  const dataNavigationHeader = document.querySelector("[data-navigation-header]");
+  // const menuBurguer = document.querySelector(".menu-burguer");
 
   $(function () {
-    $(".menu-burguer").click(function () {
+    $(".menu-burguer").click(function (e) {
+      e.stopPropagation();
+
       $(this).toggleClass("open");
 
       dataList.style.display = "none";
@@ -24,6 +25,14 @@ export default function menu() {
         dataList.style.display = "flex";
         dataNavigationHeader.style.display = "block";
       } else {
+        dataList.style.display = "none";
+        dataNavigationHeader.style.display = "none";
+        $(".menu-burguer").removeClass("open");
+      }
+    });
+
+    $(document).click(function (e) {
+      if (!$(e.target).closest(".menu-burguer, [data-list-header], [data-navigation-header]").length) {
         dataList.style.display = "none";
         dataNavigationHeader.style.display = "none";
         $(".menu-burguer").removeClass("open");
