81322563d18d29fd0760b1397f021ab0af240660
almeidaadev <unkn0wn01127800@gmail.com>
2024-07-15 22:14:46 -0300

    up

--- src/catalogo/script/header.js
+++ src/catalogo/script/header.js
@@ -1,43 +1,38 @@
 export default function menu() {
-  const dataList = document.querySelector("[data-list-header]");
-  const dataNavigationHeader = document.querySelector("[data-navigation-header]");
-  const menuBurguer = document.querySelector(".menu-burguer");
+    const dataList = document.querySelector("[data-list-header]");
+    const dataNavigationHeader = document.querySelector(
+        "[data-navigation-header]"
+    );
 
-  $(function () {
-    $(".menu-burguer").click(function (e) {
-      e.stopPropagation();
+    $(function () {
+        $("#menu-burguer, #menu-x").click(function () {
+            if ($("#menu-burguer").is(":visible")) {
+                // Menu está fechado, abrir
+                $("#menu-burguer").hide();
+                $("#menu-x").show();
 
-      $(this).toggleClass("open");
+                $(".overlay").addClass("active");
 
-      if ($(this).hasClass("open")) {
-        dataList.style.display = "flex";
-        dataNavigationHeader.style.display = "block";
-      } else {
-        dataList.style.display = "none";
-        dataNavigationHeader.style.display = "none";
-      }
+                if (dataList) {
+                    dataList.style.display = "flex";
+                }
+                if (dataNavigationHeader) {
+                    dataNavigationHeader.style.display = "block";
+                }
+            } else {
+                // Menu está aberto, fechar
+                $("#menu-burguer").show();
+                $("#menu-x").hide();
+
+                $(".overlay").removeClass("active");
+
+                if (dataList) {
+                    dataList.style.display = "none";
+                }
+                if (dataNavigationHeader) {
+                    dataNavigationHeader.style.display = "none";
+                }
+            }
+        });
     });
-
-    $(document).click(function (e) {
-      if (!menuBurguer.contains(e.target) && !dataList.contains(e.target)) {
-        $(".menu-burguer").removeClass("open");
-        if ($(window).width() <= 900) {
-          dataList.style.display = "none";
-          dataNavigationHeader.style.display = "none";
-        }
-      }
-    });
-
-    $(window).resize(function () {
-      if ($(window).width() > 900) {
-        dataList.style.display = "flex";
-        dataNavigationHeader.style.display = "block";
-      } else {
-        if (!$(".menu-burguer").hasClass("open")) {
-          dataList.style.display = "none";
-          dataNavigationHeader.style.display = "none";
-        }
-      }
-    });
-  });
 }
