02f355f57227e2cadf40f20fda1911cfe7f435a8
almeidaadev <unkn0wn01127800@gmail.com>
2024-07-15 21:59:16 -0300

    menu

--- src/scripts/internet.js
+++ src/scripts/internet.js
@@ -1,9 +1,16 @@
 export default function internet() {
-  const statusText = document.getElementById("status");
+    const statusText = document.getElementById("status");
 
-  window.addEventListener("offline", (event) => {
-    console.log("SEM INTERNET");
-    statusText.innerText = "Você está sem internet";
-    console.log(event);
-  });
+    window.addEventListener("offline", (event) => {
+        const div = document.createElement("div");
+        const divMenssage = document.createElement("divMenssage");
+        statusText.innerText = "Você está sem internet";
+        divMenssage.innerHTML = `
+          <span>Você esta sem internet</span> 
+          <br /> 
+          <span>${statusText}</span>
+        `;
+
+        div.appendChild(h1);
+    });
 }
